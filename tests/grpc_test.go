cf396c0d298bf0dba17204d874d978f2b896f3bf
haiswork <whaibin01@126.com>
2019-03-19 10:28:22 +0800

    grpc

--- tests/grpc_test.go
+++ tests/grpc_test.go
@@ -1,20 +1,66 @@
 package tests
 
-//
-//func BenchmarkGRPC_Call(b *testing.B) {
-//	addr := testGetAddr()
-//	go func() {
-//		listen, err := net.Listen("tcp", addr)
-//		if err != nil {
-//			fmt.Printf("failed to listen:%v", err)
-//		}
-//		//实现gRPC Server
-//		s := grpc.NewServer()
-//		//注册helloServer为客户端提供服务
-//		pb.RegisterHelloServer(s, HelloServer) //内部调用了s.RegisterServer()
-//		fmt.Println("Listen on" + Address)
-//
-//		s.Serve(listen)
-//	}()
-//
-//}
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = "127.0.0.1:12006"
+	grpcOnce = new(sync.Once)
+)
+
+type grpcServer struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *grpcServer) SayHello(ctx context.Context, in *HelloRequest) (*HelloReply, error) {
+	return &HelloReply{B: in.A + 1}, nil
+}
+
+func BenchmarkGRpc_Call(b *testing.B) {
+	startGRpcServer()
+
+	conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure())
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer conn.Close()
+	c := NewGRpcReqClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	b.StartTimer()
+	defer b.StopTimer()
+	req := &HelloRequest{A: 1}
+	for i := 0; i < b.N; i++ {
+		_, err := c.SayHello(ctx, req)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func startGRpcServer() {
+	grpcOnce.Do(func() {
+		go func() {
+			lis, err := net.Listen("tcp", grpcAddr)
+			if err != nil {
+				log.Fatalf("failed to listen: %v", err)
+			}
+			s := grpc.NewServer()
+			RegisterGRpcReqServer(s, &grpcServer{})
+			if err := s.Serve(lis); err != nil {
+				log.Fatalf("failed to serve: %v", err)
+			}
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+	})
+}
